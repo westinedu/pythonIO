490dee3d13dac572d1e17bd9fddff169e170c4b9
Lucas Westine <westine@gmail.com>
2018-09-06 15:25:00 +0800

    要开发的功能： []1、Html  dom 分层选    类似xpath， 不要每个都 find []2、根据关键字 搜素， 页面的 文本 框选就能定位 []3、保存网页，及网页上的下层子网页，包括所有metadata，就像另存为一样

--- crawl.py
+++ crawl.py
@@ -12,13 +12,14 @@ import warnings; warnings.simplefilter('ignore')
 V0 = 17.6639
 r = 0.01
 import pandas as pd
-
+import re
 
 # -*- coding: utf-8 -*-
 import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import copy
+import os
  
 # 体彩 排列5
 URL = "https://www.cnblogs.com/huangxie/"
@@ -30,87 +31,48 @@ crawl_content = bsObj.find("div",{"class":"forFlow"})
 
 sub_htmls = crawl_content.find_all("div",{"class":"c_b_p_desc"})
 
-def getSubHtmls(sub_htmls):
+def GetCss(html):
+    patterncss ='<link type="text/css"  href="(.*?)"'
+    patternjs = '<script src="(.*?)"'
+    patternimg = '<img src="(.*?)"'
+    patternpage = '<a.*?href="(.*?)"'
+    patternonclick = "openQuestion.*?'(.*?)'"
+    href = re.compile(patterncss, re.S).findall(html)
+    href += re.compile(patternimg, re.S).findall(html)
+    href += re.compile(patternpage, re.S).findall(html)
+    href += re.compile(patternjs, re.S).findall(html)
+    href += re.compile(patternonclick, re.S).findall(html)
+return href
+
+
+def CreateFolder(path):
+	folder = os.path.exists(path)
+	if not folder:                   #判断是否存在文件夹如果不存在则创建为文件夹
+		os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
+		print("Folder "+path+" created")
+	else:
+		print(path+"already existed")
+        
+def validateTitle(title):
+    rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
+    new_title = re.sub(rstr, "_", title)  # 替换为下划线
+    return new_title
+
+def SaveSubHtmls(sub_html_list):
     for link in sub_htmls: 
         sub_url = link.find("a").get("href")
         sub_page = urlopen(sub_url)
-        print(sub_url)   
+        #print(sub_url)   
         bsObj = BeautifulSoup(sub_page.read(),"lxml")
-        sub_file_name = sub_url.replace("://","_").replace("/","_")
+        
+        sub_file_name = validateTitle(bsObj.find("head").find("title").text+".html")
+        sub_folder_name = validateTitle(bsObj.find("head").find("title").text+"_files")
+        CreateFolder(sub_folder_name)
+        
+        print(sub_file_name)
         f = open(sub_file_name, "w",encoding='UTF-8')  
         f.write(bsObj.prettify())
        
 
-
-import os
-from bs4 import BeautifulSoup 
-import re
-siteUrls = " "
- 
-url = "http://www.sina.com.cn"
-fileNames = re.findall(r'/[^\?]*\?([^/|^\?]*)$',url)
-def getContent(url):
-	content = urlopen(url).read()
-	content = writeCss(url,content)
-	content = writefileName(url,content)
-	fileNames = re.findall(r'/[^\?]*\?([^/|^\?]*)$',url)
-	fileName = fileNames[0]
-	print(fileName)
-	f = file(fileName+".html",'w')
-	f.write(content)
-	f.close()
- 
-def writeCss(url,content):
-    soup = BeautifulSoup(content)
-    print(soup.prettify())
-    csss = soup.findAll('link',attrs={'type':'text/css'})
-    css_pat = re.compile('.*/(.*)\.css')
-    fileNames = re.findall(r'/[^\?]*\?([^/|^\?]*)$',url)
-    fileName = fileNames[0]
-    print(fileName)
-    for css in csss:
-    	cssnames = re.findall(r'.*/(.*)\.css',str(css))
-    	cssurls = re.findall(r'.*href=\"([^\"]*)\"',str(css))
-    	print(cssnames[0])
-    	print(cssurls[0])
-    	cssurl = "http://review.artintern.net/" + cssurls[0]
-    	print(cssurl)
-    	content = content.replace(cssurls[0],fileName + "/" + cssnames[0]+".css")
-    	print(os.path.isdir(fileName))
-    	if not os.path.isdir(fileName):
-    		os.mkdir(fileName)
-    	csscontent = urllib2.urlopen(cssurl).read()
-    	cssNewName = fileName+"/"+cssnames[0]+".css"
-    	cssfile = file(cssNewName,'w')
-    	cssfile.write(csscontent)
-    	cssfile.close()
-    return content
- 
-def writefileName(url,content):
-	soup = BeautifulSoup(content)
-	imgs = soup.findAll('img')
-	img_pat = re.compile('.*/(.*)\.[jpg|gif]')
-	fileNames = re.findall(r'/[^\?]*\?([^/|^\?]*)$',url)
-	fileName = fileNames[0]
-	for img in imgs:
-		imgNames = re.findall(r'.*/(.*)\.[jpg|gif]',str(img))
-		imgType = re.findall(r'.*/.*\.([^ ]*)"',str(img))
-		imgUrls = re.findall(r'.*src=\"([^\"]*)\"',str(img))
-#		print imgNames[0]
-#		print imgType[0]
-#		print imgUrls[0]
-		imgUrl = "http://review.artintern.net/" + imgUrls[0]
-#		print imgUrl
-		content = content.replace(imgUrls[0],fileName+"/"+imgNames[0]+"."+imgType[0])
-		if not os.path.isdir(fileName):
-			os.mkdir(fileName)
-		imgContent = urllib2.urlopen(imgUrl).read()
-		imgNewName = fileName+"/"+imgNames[0]+"."+imgType[0]
-		imgfile = file(imgNewName,'w')
-		imgfile.write(imgContent)
-		imgfile.close()
-	return content
- 
-getContent(url)
-
-
+print(sub_htmls)
+SaveSubHtmls(sub_htmls)
