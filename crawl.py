490fbdd34bbda6994b5c5087b14e13a682a71f08
Lucas Westine <westine@gmail.com>
2018-09-10 16:29:50 +0800

    logging and js dynamically catch

--- crawl.py
+++ crawl.py
@@ -13,13 +13,14 @@ V0 = 17.6639
 r = 0.01
 import pandas as pd
 import re
-
+import logging
 # -*- coding: utf-8 -*-
 import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import copy
 import os
+import time
  
 # 体彩 排列5
 URL = "https://www.cnblogs.com/huangxie/"
@@ -31,19 +32,44 @@ crawl_content = bsObj.find("div",{"class":"forFlow"})
 
 sub_htmls = crawl_content.find_all("div",{"class":"c_b_p_desc"})
 
-def GetCss(html):
-#    patterncss ='<link[]type="text/css"  href="(.*?)"'
-    patterncss = r'\.css'
+
+def initLogging(logFilename):
+  """Init for logging
+  """
+  logging.basicConfig(
+                    level    = logging.DEBUG,
+                    format='%(asctime)s-%(levelname)s-%(message)s',
+                    datefmt  = '%y-%m-%d %H:%M',
+                    filename = logFilename,
+                    filemode = 'w');
+  console = logging.StreamHandler()
+  console.setLevel(logging.INFO)
+  formatter = logging.Formatter('%(asctime)s-%(levelname)s-%(message)s')
+  console.setFormatter(formatter)
+  logging.getLogger('').addHandler(console)
+
+def GetCss(soup_html):
+#    patterncss =r'<link\wtype="text/css"\whref="(.*?)"'
+    #patterncss = r'\.css'
 #    patternjs = '<script src="(.*?)"'
 #    patternimg = '<img src="(.*?)"'
 #    patternpage = '<a.*?href="(.*?)"'
 #    patternonclick = "openQuestion.*?'(.*?)'"
-    href = re.compile(patterncss, re.S).findall(html)
+#    href = re.compile(patterncss, re.S).findall(html)
 #    href += re.compile(patternimg, re.S).findall(html)
 #    href += re.compile(patternpage, re.S).findall(html)
 #    href += re.compile(patternjs, re.S).findall(html)
 #    href += re.compile(patternonclick, re.S).findall(html)
-    return href
+    for k in  soup_html.find_all(href=re.compile("css")):
+        print(k.get("href"))
+
+def GetJs(soup_html):
+    for k in  soup_html.find_all(src=re.compile("js")):
+        print(k.get("src"))
+
+def GetImg(soup_html):
+    for k in  soup_html.find_all("img"):
+        print(k.get("src"))
 
 
 def CreateFolder(path):
@@ -52,7 +78,7 @@ def CreateFolder(path):
 		os.makedirs(path)            #makedirs 创建文件时如果路径不存在会创建这个路径
 		print("Folder "+path+" created")
 	else:
-		print(path+"already existed")
+		print(path+" already existed")
         
 def validateTitle(title):
     rstr = r"[\/\\\:\*\?\"\<\>\|]"  # '/ \ : * ? " < > |'
@@ -65,7 +91,11 @@ def SaveSubHtmls(sub_html_list):
         sub_page = urlopen(sub_url)
         #print(sub_url)   
         bsObj = BeautifulSoup(sub_page.read(),"lxml")
-        print(GetCss(bsObj.prettify()))
+#        print(bsObj.find("head").prettify())
+        GetCss(bsObj.find("head"))
+        GetJs(bsObj.find("head"))
+        GetImg(bsObj)
+        logging.info(bsObj.prettify())
         
         sub_file_name = validateTitle(bsObj.find("head").find("title").text+".html")
         sub_folder_name = validateTitle(bsObj.find("head").find("title").text+"_files")
@@ -74,7 +104,8 @@ def SaveSubHtmls(sub_html_list):
         print(sub_file_name)
         f = open(sub_file_name, "w",encoding='UTF-8')  
         f.write(bsObj.prettify())
+        time.sleep(5)
        
-
-print(sub_htmls)
+initLogging('test.log')
+#print(sub_htmls)
 SaveSubHtmls(sub_htmls)
